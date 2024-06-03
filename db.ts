cd0225f956631106e0111a363c30d0070212127c
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-06-03 12:03:54 +0200

    failing to command sqlite3 to read DBsetup.sql

--- db.ts
+++ db.ts
@@ -6,15 +6,24 @@ let database: Database;
 
 export const startDatabase = async () => {
   if (!database) {
-    database = await sqlite.open({
-      driver: sqlite3.Database,
-      filename: "database.sqlite",
-    });
+		database = await sqlite.open({
+			driver: sqlite3.Database,
+			filename: "database.sqlite",
+		});
 
-    await database.run("PRAGMA foreign_keys = ON");
+		await database.run("PRAGMA foreign_keys = ON");
+		console.log("Empty database created");
 
-    console.log("Database ready");
-  }
+		/* trying off the top of my head, didn't work [clownEmoji] */
+		try {
+			await database.run(".read DBsetup.sql");
+			console.log("Database ready");
+		} catch (error) {
+			console.log("Couldn't read DBsetup.sql");
+			console.error(error);
+		}
+		/* syntax error */
+	}
 
   return database;
 };
