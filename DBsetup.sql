dc44bafb51bbf1450c7f437d3a87eaa878ee6411
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-06-03 12:02:51 +0200

    Placeholder posts and comments removed from the setup

--- DBsetup.sql
+++ DBsetup.sql
@@ -70,8 +70,7 @@ INSERT INTO forums (name,description) VALUES ('games', 'place to discuss games')
 INSERT INTO forums (name,description) VALUES ('movies', 'all the niche movie references go here, bring your spoons');
 INSERT INTO forums (name,description) VALUES ('animals', 'do they deserve us?');
 
-
-INSERT INTO posts (author, forum, title, content) VALUES (1,1,'First post' ,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+/* INSERT INTO posts (author, forum, title, content) VALUES (1,1,'First post' ,'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 
@@ -128,3 +127,4 @@ Thanks for any help
 <3');
 
 UPDATE posts SET comment_amount=2 WHERE id=3;
+ */
