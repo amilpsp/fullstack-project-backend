33ef0b03e06c66bd2cf8bd0f62182c6b558d940a
celoob <carlemilloob@gmail.com>
2024-06-04 11:15:25 +0200

    Added a comment (i know backend)

--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
-import cors from "cors";
-import express, { Request } from "express";
-import { startDatabase } from "./db";
-import postsRouter from "./routes/posts";
-import forumsRouter from "./routes/forums";
-import loginRouter from "./routes/login";
-import usersRouter from "./routes/users";
-import commentsRouter from "./routes/comments";
-import signupRouter from "./routes/signup";
+import cors from 'cors';
+import express, { Request } from 'express';
+import { startDatabase } from './db';
+import postsRouter from './routes/posts';
+import forumsRouter from './routes/forums';
+import loginRouter from './routes/login';
+import usersRouter from './routes/users';
+import commentsRouter from './routes/comments';
+import signupRouter from './routes/signup';
 const port = process.env.PORT || 8080;
 
 const app = express();
@@ -20,20 +20,30 @@ app.use(cors<Request>());
 (async () => {
   await startDatabase();
 
-  app.use("/posts", postsRouter);
-  app.use("/forums", forumsRouter);
-  app.use("/login", loginRouter);
-  app.use("/users", usersRouter);
-  app.use("/signup", signupRouter);
-  app.use("/comments", commentsRouter);
+  app.use('/posts', postsRouter);
+  app.use('/forums', forumsRouter);
+  app.use('/login', loginRouter);
+  app.use('/users', usersRouter);
+  app.use('/signup', signupRouter);
+  app.use('/comments', commentsRouter);
   app.listen(port);
 })();
 
 //// TO RUN DBSETUP.SQL WITH CHANGES
 /*
+--MAC--
 in terminal:
 sqlite3
 .mode markdown
 PRAGMA foreign_keys=ON;
 .read DBsetup.sql
+
+
+--WINDOWS--
+in terminal (bash):
+winpty sqlite3
+.mode markdown
+PRAGMA foreign_keys=ON;
+.read DBsetup.sql
+
 */
